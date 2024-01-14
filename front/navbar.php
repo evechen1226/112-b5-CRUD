18a7e6c27c7ca53a94022650114e5fec41fafd09
evechen1226 <eveliving2021@gmail.com>
2024-01-14 13:17:54 +0800

    增加index2.php--測試增加功能

---
+++ front/navbar.php
@@ -0,0 +1,31 @@
+        <div class="container-xxl position-relative p-0">
+          <nav class="navbar navbar-expand-lg navbar-dark bg-dark px-4 px-lg-5 py-3 py-lg-0">
+            <a href="" class="navbar-brand p-0">
+              <h1 class="text-primary m-0 title-font nav-icon"><i class="fa-solid fa-seedling me-3"></i>Green
+                Planet</h1>
+              <!-- <img src="img/logo.png" alt="Logo"> -->
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+              <span class="fa fa-bars"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarCollapse">
+              <div class="navbar-nav ms-auto py-0 pe-4 nav-text">
+                <a href="index.html" class="nav-item nav-link active">Home</a>
+                <a href="about.html" class="nav-item nav-link">關於我們</a>
+                <a href="service.html" class="nav-item nav-link">提供服務</a>
+                <a href="menu.html" class="nav-item nav-link">餐點選單</a>
+                <div class="nav-item dropdown">
+                  <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Pages</a>
+                  <div class="dropdown-menu m-0">
+                    <a href="booking.html" class="dropdown-item">Booking</a>
+                    <a href="team.html" class="dropdown-item">Our Team</a>
+                    <a href="testimonial.html" class="dropdown-item">Testimonial</a>
+                  </div>
+                </div>
+                <a href="contact.html" class="nav-item nav-link">聯絡我們</a>
+              </div>
+              <a href="" class="btn btn-primary py-2 px-4">訂位資訊</a>
+            </div>
+          </nav>
+
+        </div>
