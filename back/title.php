91dc244f1566d86e5753ecad57d8b150edf195a2
evechen1226 <eveliving2021@gmail.com>
2024-01-17 16:03:43 +0800

    back/title.php--增加bootstrap tag 排版

--- back/title.php
+++ back/title.php
@@ -1,30 +1,65 @@
 <style>
-	img{
+	img {
 		width: 200px;
 		height: 100px;
+	}
+
+	.form-check-input {
+		scale: 1.2;
+		border: 1px solid lightslategray;
+	}
+
+	.btn-green {
+		background-color: #54deca;
+	}
+
+	.btn-green:hover {
+		background-color: #1E9D8A;
+		color: white;
+	}
+
+	.btn-green:active {
+		background-color: #1E9D8A !important;
+		color: white !important;
+	}
+
+	.form-check-input:checked[type="checkbox"] {
+		background-color: #1E9D8A;
+
+	}
+
+	.form-check-input:checked[type="radio"] {
+		background-color: #1E9D8A;
 	}
 </style>
 <div>
 	<h3>網站標題管理</h3>
 	<!-- <form method="post" target="back" action="?do=tii"> -->
+
+
+			
+				<input class="btn btn-green mx-end" type="button" onclick="op('#cover','#cvr','./modal/<?= $do; ?>.php?table=<?= $do; ?>')" value="新增網站標題圖片">
+
+
+
 	<form method="post" action="../api/edit.php">
-		<table width="100%" class="table table-striped text-center">
+		<table width="100%" class="table table-striped text-center align-middle">
 			<tbody>
 				<thead>
 					<tr">
-						<th class="col text-center">網站標題</th>
-						<th class="col text-center">替代文字</th>
-						<th class="col text-center">顯示</th>
-						<th class="col text-center">刪除</th>
-						<th class="col text-center"></th>
-					</tr>
+						<th class="col">網站標題</th>
+						<th class="col">替代文字</th>
+						<th class="col">顯示</th>
+						<th class="col">刪除</th>
+						<th class="col"></th>
+						</tr>
 				</thead>
 				<?php $rows = $Title->all();
 				foreach ($rows as $row) {
 				?>
 					<tr>
 						<td>
-							<img src="./img/<?= $row['img']; ?>" alt="">
+							<img class="rounded" src="./img/<?= $row['img']; ?>" alt="">
 						</td>
 
 						<td>
@@ -32,8 +67,8 @@
 							<input type="hidden" name="id[]" value="<?= $row['id']; ?>">
 						</td>
 
-						<td>
-							<input class="form-check-input mt-0" type="radio" name="sh[]" value="<?= $row['id']; ?>" id="">
+						<td class="">
+							<input class="form-check-input mt-0 " type="radio" name="sh[]" value="<?= $row['id']; ?>" id="">
 						</td>
 
 						<td>
@@ -41,21 +76,21 @@
 						</td>
 
 						<td>
-							<input class="" type="button" onclick="op('#cover','#cvr','./modal/upload.php?table=<?= $do; ?>&id=<?= $row['id']; ?>')" value="更新圖片">
+							<input class="btn btn-green" type="button" onclick="op('#cover','#cvr','./modal/upload.php?table=<?= $do; ?>&id=<?= $row['id']; ?>')" value="更新圖片">
 						</td>
 					</tr>
 				<?php
 				} ?>
 			</tbody>
 		</table>
-		<table style="margin-top:40px; width:70%;">
-			<tbody>
-				<tr>
-					<input type="hidden" name="table" value="<?= $do; ?>">
-					<td width="200px"><input type="button" onclick="op('#cover','#cvr','./modal/<?= $do; ?>.php?table=<?= $do; ?>')" value="新增網站標題圖片"></td>
-					<td class="cent"><input type="submit" value="修改確定"><input type="reset" value="重置"></td>
-				</tr>
-			</tbody>
-		</table>
+		<div class="container text-center">
+			<div class="row">
+				<div class="col">
+					<input class="btn btn-warning" type="submit" value="修改確定">
+					<input class="btn btn-warning" type="reset" value="重置">
+				</div>
+			</div>
+		</div>
+		<input type="hidden" name="table" value="<?= $do; ?>">
 	</form>
 </div>
