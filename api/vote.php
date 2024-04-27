ecad0af5fd26317226ac9b570e7bec3bbf11319d
evechen1226 <eveliving2021@gmail.com>
2024-04-28 07:21:45 +0800

    fix:api/vote.php--刪除多餘dd()

--- api/vote.php
+++ api/vote.php
@@ -1,11 +1,10 @@
 <?php include 'db.php';
-dd($_POST);
+
 $row = $Vote->find($_POST['id']);
-dd($row);
 $row['vote']=$row['vote']+1;
 $Vote->save($row);
+
 $mem=$Admin->find(['id'=>$_SESSION['id']]);
-dd($mem);
 $mem['vote']=$row['id'];
 $Admin->save($mem);
 to('../index.php');
