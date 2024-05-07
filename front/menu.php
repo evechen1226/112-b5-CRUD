fc9899ab17dde52c8411776b2bff828cfa12cd9a
evechen1226 <eveliving2021@gmail.com>
2024-05-07 18:58:53 +0800

    fix:front/menu.php--將more的icon刪除

--- front/menu.php
+++ front/menu.php
@@ -29,7 +29,7 @@
                     ?>
                       <div class="col-lg-6">
                         <div class="d-flex align-items-center">
-                          <img class="flex-shrink-0 img-fluid rounded" src="img/menu-1.jpg" alt="" style="width: 80px;">
+                          <!-- <img class="flex-shrink-0 img-fluid rounded" src="img/menu-1.jpg" alt="" style="width: 80px;"> -->
                           <div class="w-100 d-flex flex-column text-start ps-4">
                             <h5 class="d-flex justify-content-between border-bottom pb-2">
                               <span><?= $row['title']; ?></span>
@@ -52,7 +52,7 @@
               <div class="text-center">
                 <?php
                 if (!isset($_GET['p']) && $total > $div) {
-                  echo '<a class="fs-4" href="news.php?p=1">more<i class="fa-solid fa-angles-right"></i></a>';
+                  echo '<a class="fs-4" href="news.php?p=1">more</i></a>';
                 } else {
 
                   if ($now >= 1) {
